72d579c189f056488b5b48fff33b5960e28d673f
jasim00021 <sk.jasimuddin@techwens.in>
2023-09-19 15:10:19 +0530

    splash_screen

--- app/src/main/java/com/jasim0021/splash_demo/SplashScreen.java
+++ app/src/main/java/com/jasim0021/splash_demo/SplashScreen.java
@@ -2,7 +2,9 @@ package com.jasim0021.splash_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Intent i = new Intent(SplashScreen.this,MainActivity.class);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },3000);
+
+
     }
 }
