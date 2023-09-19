662484724f9a870a96e36fbe9410881b7326f353
jasim00021 <sk.jasimuddin@techwens.in>
2023-09-19 15:09:50 +0530

    splash_screen

---
+++ app/src/main/java/com/jasim0021/splash_demo/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.jasim0021.splash_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
