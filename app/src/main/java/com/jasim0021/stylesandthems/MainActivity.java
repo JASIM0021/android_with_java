f7701e9ed47737b74f18c81a5be279fa30757f4e
jasim00021 <sk.jasimuddin@techwens.in>
2023-09-21 21:59:44 +0530

    styles and thems in android

---
+++ app/src/main/java/com/jasim0021/stylesandthems/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jasim0021.stylesandthems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
