bd91933e7563f1f64509f486006bdf9c4f23f5f1
jasim00021 <sk.jasimuddin@techwens.in>
2023-09-21 21:36:46 +0530

    Spiner / dropdown using listview

--- app/src/main/java/com/jasim0021/listview_demo/MainActivity.java
+++ app/src/main/java/com/jasim0021/listview_demo/MainActivity.java
@@ -7,14 +7,20 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    Spinner spinner;
+    AutoCompleteTextView autoCompleteTextView;
     ArrayList<String> arrNames = new ArrayList<>();
+    ArrayList<String> arrIdproof = new ArrayList<>();
+    ArrayList<String> arr_city = new ArrayList<>();
 
     ArrayAdapter<String> adapter ;
     @Override
@@ -23,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
+        spinner = findViewById(R.id.spiner_list);
+        autoCompleteTextView = findViewById(R.id.auto_Complete);
 
         arrNames.add("Jasim");
         arrNames.add("Jasim");
@@ -40,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         arrNames.add("Jasim");
         arrNames.add("Jasim");
         arrNames.add("Jasim");
+
+
 
         adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,arrNames);
 
@@ -52,6 +62,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
+//        Spiner
+        arrIdproof.add("Adhar");
+        arrIdproof.add("Voter");
+        arrIdproof.add("Pan");
+        arrIdproof.add("Driving");
+        arrIdproof.add("Polio");
+        arrIdproof.add("Admit");
+
+        ArrayAdapter spinerAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,arrIdproof);
+        spinner.setAdapter(spinerAdapter);
+
+
+//        AutoComplete
+
+
 
     }
 }
