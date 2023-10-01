2f37c505ed2ebc4d5fd30fff3dfb288f52b69f5a
jasim0021 <sk.jasimuddin9064@gmail.com>
2023-10-01 21:59:09 +0530

    intent/activity opening using notification

--- app/src/main/java/com/jasim0021/notificationexample/MainActivity.java
+++ app/src/main/java/com/jasim0021/notificationexample/MainActivity.java
@@ -1,11 +1,16 @@
 package com.jasim0021.notificationexample;
 
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static  final  String CHANEL_ID="Channel ID Here";
     private static  final  int NOTIFICATION_ID=100;
+    private static  final  int REQUEST_CODE=101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +32,19 @@ public class MainActivity extends AppCompatActivity {
         BitmapDrawable bitmapDrawable = (BitmapDrawable)drawable;
         Bitmap largeIcon = bitmapDrawable.getBitmap();
         Notification notification;
+        Intent notiFicationIntend = new Intent(this, MainActivity.class);
+//        clear the previus same screen if user alrady present in the app
+        notiFicationIntend.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,REQUEST_CODE,notiFicationIntend,PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
              notification = new Notification.Builder(this)
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.ic_launcher_foreground)
                     .setContentText("New Message")
                     .setSubText("New Message from jasim")
+                     .setContentIntent(pendingIntent)
                     .setChannelId(CHANEL_ID)
                     .build();
              nm.createNotificationChannel(new NotificationChannel(CHANEL_ID,"New Channel",NotificationManager.IMPORTANCE_HIGH));
@@ -40,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.ic_launcher_background)
                     .setContentText("New Message")
+                    .setContentIntent(pendingIntent)
                     .setSubText("New Message from jasim")
 
                     .build();
